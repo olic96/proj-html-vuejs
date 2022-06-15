c716cbb23aea98d149764db0bdc8d39257a949ec
olic96 <francoolich@gmail.com>
2022-06-15 12:48:57 +0200

    scaf + logo

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="container">
+    <img class="logo" src="../../fable/images/logo.png" alt="logo">
+    <!-- <CardNav/> -->
+  </header>
+</template>
+
+<script>
+// import CardNav from './commons/CardNav.vue';
+export default {
+    // name: CardNav,
+}
+</script>
+
+<style lang="scss">
+header {
+    display: flex;
+    height: 6.25rem;
+}
+
+.logo {
+    height: 100%;
+}
+</style>

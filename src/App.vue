1391bcfd970530aacd725fe3497ad37730cfee8e
olic96 <francoolich@gmail.com>
2022-06-15 11:53:37 +0200

    scaf

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <BaseHeader/>
+    <BaseMain/>
+    <BaseFooter/>
+  </div>
+</template>
+
+<script>
+import BaseHeader from './components/blocks/BaseHeader.vue';
+import BaseMain from './components/blocks/BaseMain.vue';
+import BaseFooter from './components/blocks/BaseFooter.vue';
+export default {
+  name: 'App',
+  components: {
+    BaseHeader,
+    BaseMain,
+    BaseFooter
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'assets/style/global.scss';
+</style>

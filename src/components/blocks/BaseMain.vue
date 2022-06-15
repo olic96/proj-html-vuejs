c133858c0acd687dd046bcab9e1abe21c64ae834
olic96 <francoolich@gmail.com>
2022-06-15 17:09:36 +0200

    section 1

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -1,14 +1,16 @@
 <template>
   <main>
     <BaseHero/>
+    <BaseCardSection1/>
   </main>
 </template>
 
 <script>
+import BaseCardSection1 from '../commons/BaseCardSection1.vue'
 import BaseHero from '../sections/BaseHero.vue'
 
 export default {
-  components: { BaseHero },
+  components: { BaseHero, BaseCardSection1 },
   name: 'BaseMain',
 }
 </script>
---
+++ src/components/commons/BaseCardSection1.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+      <div class="container">
+          <div class="col-12">
+              <h1>Lorem ipsum</h1>
+              <p><small>Lorem ipsum dolor sit amet</small></p>
+              <img src="../../fable/images/header_divider.png">
+          </div>
+          <div class="col-3">
+              <!-- v-for -->
+              <div class="col_circle">
+                  <img src="">
+                  <h5></h5>
+                  <p></p>
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name:'BaseCardSection1',
+}
+</script>
+
+<style lang='scss' scoped>
+    .container {
+        text-align: center;
+    }
+</style>

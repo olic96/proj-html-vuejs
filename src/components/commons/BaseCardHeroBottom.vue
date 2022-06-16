944d71b5a2f2c0d147058c62785cc9bc09091c3a
olic96 <francoolich@gmail.com>
2022-06-16 16:36:01 +0200

    hero

--- src/components/commons/BaseCardHeroBottom.vue
+++ src/components/commons/BaseCardHeroBottom.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hero_bottom">
       <div class="container">
-          <div>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          </div>
+          <h2>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h2>
           <div>
               <button>
-                  LEARN MORE
+                  LEARN MORE <i class="fa-solid fa-angle-right"></i>
               </button>
           </div>
       </div>
@@ -23,9 +23,22 @@ export default {
     .container {
         display: flex;
         justify-content: space-between;
+        align-items: center;
     }
     .hero_bottom {
         height: 110px;
         background-image: url("../../fable/images/pattern.png");
+        color: var(--third-color);
+    }
+
+    h2, button {
+        margin-top: 30px;
+    }
+
+    button {
+        border: 1px solid var(--third-color);
+        color: var(--third-color);
+        padding: 10px 15px;
+        background: transparent;
     }
 </style>

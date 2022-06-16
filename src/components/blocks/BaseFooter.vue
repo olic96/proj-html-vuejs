1c5930dd9018f9c0df9a238577222a04f95146b1
olic96 <francoolich@gmail.com>
2022-06-16 11:59:19 +0200

    footer

--- src/components/blocks/BaseFooter.vue
+++ src/components/blocks/BaseFooter.vue
@@ -1,15 +1,28 @@
 <template>
   <footer>
-      
+      <div class="footer_top">
+        <div class="container">
+          <div class="row">
+            <div class="col-3">
+              <!-- v-for card -->
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer_bottom"></div>
   </footer>
 </template>
 
 <script>
 export default {
-
+  name:'BaseFooter',
 }
 </script>
 
 <style>
-
+  .footer_top {
+    height: 400px;
+    background-image: url("../../fable/images/pattern.png");
+  }
 </style>

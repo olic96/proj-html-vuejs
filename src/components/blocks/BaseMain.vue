6ac36ddc9076857210e815094eacb68365a06651
olic96 <francoolich@gmail.com>
2022-06-16 10:30:09 +0200

    section5

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -5,6 +5,7 @@
     <BaseSection2/>
     <BaseSection3/>
     <BaseSection4/>
+    <BaseSection5/>
   </main>
 </template>
 
@@ -14,10 +15,11 @@ import BaseHero from '../sections/BaseHero.vue'
 import BaseSection2 from '../sections/BaseSection2.vue'
 import BaseSection3 from '../sections/BaseSection3.vue'
 import BaseSection4 from '../sections/BaseSection4.vue'
+import BaseSection5 from '../sections/BaseSection5.vue'
 
 
 export default {
-  components: { BaseHero, BaseSection1, BaseSection2, BaseSection3, BaseSection4 },
+  components: { BaseHero, BaseSection1, BaseSection2, BaseSection3, BaseSection4, BaseSection5 },
   name: 'BaseMain',
 }
 </script>
---
+++ src/components/sections/BaseSection5.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+      <div class="container">
+          <div class="col-6">
+              <h4></h4>
+              <p></p>
+              <ul>
+                  <li></li>
+              </ul>
+          </div>
+          <div class="col-6">
+              <!-- v-for -->
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name:'BaseSection5',
+}
+</script>
+
+<style>
+
+</style>
